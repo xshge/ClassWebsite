1ceb95e0cb0e0e8bc2514f4ab286e108251cf942
xshenge <xshen12138@outlook.com>
2025-01-29 22:18:30 -0500

    mostly works

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.getElementById("chSt").addEventListener("mouseover", function () {
 
     sum.style.display = "block";
     sum.style.opacity = "1";
-    sum.style.visibility = "visible";
+
 
 })
 
@@ -16,6 +16,45 @@ document.getElementById("chSt").addEventListener("mouseout", function () {
 
     if (sum.style.display == "block") {
         sum.style.display = "none";
+        sum.style.opacity = "0";
+    }
+
+
+})
+
+document.getElementById("genTl").addEventListener("mouseover", function () {
+    sum = document.getElementById("sum-two");
+    sum.style.display = "block";
+    sum.style.opacity = "1";
+
+})
+
+document.getElementById("genTl").addEventListener("mouseout", function () {
+
+    sum = document.getElementById("sum-two");
+
+    if (sum.style.display == "block") {
+        sum.style.display = "none";
+        sum.style.opacity = "0";
+    }
+
+
+})
+
+document.getElementById("sR").addEventListener("mouseover", function () {
+    sum = document.getElementById("sum-three");
+    sum.style.display = "block";
+    sum.style.opacity = "1";
+
+})
+
+document.getElementById("sR").addEventListener("mouseout", function () {
+
+    sum = document.getElementById("sum-three");
+
+    if (sum.style.display == "block") {
+        sum.style.display = "none";
+        sum.style.opacity = "0";
     }
 
 
